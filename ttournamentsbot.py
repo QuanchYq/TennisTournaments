4300739b38709384615e026fa633a1c52c07a22f
Aikyn <88246377+QuanchYq@users.noreply.github.com>
2023-04-05 14:55:52 +0500

    Update ttournamentsbot.py

--- ttournamentsbot.py
+++ ttournamentsbot.py
@@ -10,6 +10,8 @@ from aiogram.utils import executor
 import json
 import keyboards as kb
 import libs
+eventsdb = sqlite3.connect('events.db')
+events = eventsdb.cursor()
 
 bot_token = "5902246169:AAH6BRVP1ztgt_BjGtbQbbtUjgS3JvQmq6M"
 bot_username = "ttournamentsbot"
@@ -424,5 +426,45 @@ async def process_continue(callback_query: types.CallbackQuery, state: FSMContex
                 f"<b>Название:</b> {data['name']}\n<b>Дата:</b> {data['date']}\n<b>Ссылка:</b> {data['link']}\n<b>Категория:</b> {data['category']}\n<b>Сумма взноса:</b> {data['price']}\n<b>Что входит:</b> {data['addition']}\n<b>Имя организатор:</b> {data['organizer_name']}\n<b>Телефон организатора: </b> {data['organizer_phone']}\n<b>Место проведения:</b> {data['place']}\n<b>Другие комментарии:</b> {data['comments']}\n<b>Фото:</b> {photos_count} фото\n<b>Файл:</b> {files_count} файл", reply_markup=kb.confirmButton, disable_web_page_preview=True)
 
 
+
+
+async def mail():
+    users = libs.getUsers()
+    todays_events = events.execute(f"SELECT * FROM events WHERE date(date) = '{datetime.date.today()}'").fetchall()
+    for event in todays_events:
+        message = libs.getEventMessage(event)
+        for user in users:
+            if user[9] != event[4] and user[9] != '':
+                continue
+            try:
+                if event[11] == '':
+                    await bot.send_message(user[1], message)
+                else:
+                    photos = json.loads(event[11])
+                    media = []
+                    for index, photo in enumerate(photos):
+                        media.append(types.InputMediaPhoto(photo, message) if index == 0 else types.InputMediaPhoto(photo))
+                    await bot.send_media_group(user[1], media)
+                if event[12] != '':
+                    files = json.loads(event[12])
+                    media = []
+                    for index, file in enumerate(files):
+                        media.append(types.InputMediaDocument(file))
+                    await bot.send_media_group(user[1], media)
+            except BotBlocked as E:
+                libs.removeUser(user[0])
+            except:
+                pass
+            await asyncio.sleep(0.1)
+    libs.usersVacuum()
+
+
+# Schedule the mail() function to run every day at 00:10
+schedule.every().day.at("15:35").do(asyncio.run, mail())
+
+# Run the scheduler loop
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
